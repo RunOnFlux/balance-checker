fd4716087a9f0614a1496b7df77683531698936e
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-05-04 10:23:18 +0700

    codebase

--- discord/hooks.js
+++ discord/hooks.js
@@ -1,27 +1,26 @@
 const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook(`${process.env.WEB_HOOK}`);
 
+const hook = new Webhook(`${process.env.WEB_HOOK}`);
 
 const IMAGE_URL = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
 hook.setUsername('Discord Webhook Balance Bot Test');
 hook.setAvatar(IMAGE_URL);
 
-
-function checkHook(item, explorer) {
-    if (item.ALERT) {
-        if (item.balance < item.ALERT) {
-            sendHook(item.coin, item.label, item.address, item.balance, item.ALERT, explorer);
-        }
-    }
+async function sendHook(coin, label, address, balance, alertamount, explorer) {
+  hook.send('@everyone');
+  const title = `${label} address ***${address}*** is below the alert balance.
+          \nDeposit ${coin} ASAP
+          \n`;
+  const value = `${balance} < ${alertamount}\n${explorer}${address}`;
+  hook.warning(`**${coin} Balance Warning**`, title, value);
 }
 
-async function sendHook(coin, label, address, balance, alertamount, explorer) {
-    hook.send('@everyone');
-    var title = `${label} address ***${address}*** is below the alert balance.
-        \nDeposit ${coin} ASAP
-        \n`;
-    var value = `${balance} < ${alertamount}\n${explorer}${address}`;
-    hook.warning(`**${coin} Balance Warning**`, title, value);
+function checkHook(item, explorer) {
+  if (item.ALERT) {
+    if (item.balance < item.ALERT) {
+      sendHook(item.coin, item.label, item.address, item.balance, item.ALERT, explorer);
+    }
+  }
 }
 
 // hook.send('Testing discord send. Sorry');
--- server.js
+++ server.js
@@ -1,37 +1,22 @@
-var http = require('http');
-var request = require('request');
+const http = require('http');
+const config = require('config');
+const app = require('./src/lib/server');
+const log = require('./src/lib/log');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const rpcMethods = require("./routes/api");
+const balanceService = require('./src/services/balanceService');
 
-const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || config.server.port;
 
-app.use(bodyParser.urlencoded({extended: false }));
-app.use(bodyParser.json());
+balanceService.fetchBalances();
+setInterval(() => {
+  balanceService.fetchBalances();
+}, 5 * 60 * 1000);
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
+setInterval(() => {
+  balanceService.checkHooks();
+}, 6 * 60 * 1000);
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+server.listen(port, () => {
+  log.info(`App listening on port ${port}!`);
 });
-
-app.use("/api", rpcMethods);
-
-const port = process.env.PORT || 4444
-
-server = app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${port}`));
